83eacdc42ea34e66438543421c3950af238eac45
JmKanmo <nebi25@naver.com>
2020-06-24 03:37:30 +0900

    프로젝트A 리뷰반영(main.xml, activity)

--- app/src/main/java/com/example/mymovie/activities/MainActivity.java
+++ app/src/main/java/com/example/mymovie/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mymovie.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,26 +11,108 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.mymovie.R;
-import com.example.mymovie.utils.Listeners.MainListenerController;
 
 public class MainActivity extends AppCompatActivity {
-    private MainListenerController viewEventListenerController;
+    private Button thumbUpBtn;
+    private Button thumbDownBtn;
+    private TextView thumbUpCnt;
+    private TextView thumbDownCnt;
+    private LinearLayout reviewWriteBtn;
+    private Button seeAllBtn;
+    private ImageView loginFacebookBtn;
+    private ImageView loginKakaoBtn;
+    private Button purchaseBtn;
 
-    private void initListener() {
-        this.viewEventListenerController = MainListenerController.builder().thumbDownBtn((Button) findViewById(R.id.thumb_down_btn))
-                .thumbUpBtn((Button) findViewById(R.id.thumb_up_btn))
-                .thumbDownCnt((TextView) findViewById(R.id.thumb_down_cnt))
-                .thumbUpCnt((TextView) findViewById(R.id.thumb_up_cnt)).reviewWriteBtn((LinearLayout) findViewById(R.id.review_write_btn))
-                .seeAllBtn((Button) findViewById(R.id.see_all_btn)).loginFacebookBtn((ImageView) findViewById(R.id.login_facebook_btn))
-                .loginKakaoBtn((ImageView) findViewById(R.id.login_kakao_btn)).purchaseBtn((Button) findViewById(R.id.purchase_btn)).build();
+    private void initView() {
+        this.thumbUpBtn = findViewById(R.id.thumb_up_btn);
+        this.thumbDownBtn = findViewById(R.id.thumb_down_btn);
+        this.thumbUpCnt = findViewById(R.id.thumb_up_cnt);
+        this.thumbDownCnt = findViewById(R.id.thumb_down_cnt);
+        this.reviewWriteBtn = findViewById(R.id.review_write_btn);
+        this.seeAllBtn = findViewById(R.id.see_all_btn);
+        this.loginFacebookBtn = findViewById(R.id.login_facebook_btn);
+        this.loginKakaoBtn = findViewById(R.id.login_kakao_btn);
+        this.purchaseBtn = findViewById(R.id.purchase_btn);
+    }
 
-        this.viewEventListenerController.setOnClickListener();
+    private void initEventListener() {
+        this.thumbUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (thumbUpBtn.isSelected()) {
+                    thumbUpBtn.setSelected(false);
+                    thumbUpCnt.setText(String.valueOf(Integer.parseInt(thumbUpCnt.getText().toString()) - 1));
+                } else {
+                    thumbUpBtn.setSelected(true);
+                    thumbUpCnt.setText(String.valueOf(Integer.parseInt(thumbUpCnt.getText().toString()) + 1));
+
+                    if (thumbDownBtn.isSelected()) {
+                        thumbDownBtn.setSelected(false);
+                        thumbDownCnt.setText(String.valueOf(Integer.parseInt(thumbDownCnt.getText().toString()) - 1));
+                    }
+                }
+            }
+        });
+
+        this.thumbDownBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (thumbDownBtn.isSelected()) {
+                    thumbDownBtn.setSelected(false);
+                    thumbDownCnt.setText(String.valueOf(Integer.parseInt(thumbDownCnt.getText().toString()) - 1));
+                } else {
+                    thumbDownBtn.setSelected(true);
+                    thumbDownCnt.setText(String.valueOf(Integer.parseInt(thumbDownCnt.getText().toString()) + 1));
+
+                    if (thumbUpBtn.isSelected()) {
+                        thumbUpBtn.setSelected(false);
+                        thumbUpCnt.setText(String.valueOf(Integer.parseInt(thumbUpCnt.getText().toString()) - 1));
+                    }
+                }
+            }
+        });
+
+        this.loginFacebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Log", "loginfacebookbtn click");
+            }
+        });
+
+        this.loginKakaoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Log", "loginKakaobtn click");
+            }
+        });
+
+        this.reviewWriteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Log", "reviewWriteBtn click");
+            }
+        });
+
+        this.purchaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Log", "purchaseBtn click");
+            }
+        });
+
+        this.seeAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Log", "seeAllBtn click");
+            }
+        });
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.initListener();
+        this.initView();
+        this.initEventListener();
     }
 }
